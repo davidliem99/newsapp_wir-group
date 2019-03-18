9a63cf60c8ad50b57ca56293854fae03a434e20b
merurend <merurw@gmail.com>
2019-03-18 19:54:06 +0700

    test sw1

--- src/actions/news.js
+++ src/actions/news.js
@@ -6,15 +6,26 @@ const NEWSAPI_KEY = "8fd304b52fab4e2cb6edb2c0a7d12251"
 
 export const getNews = (category) => {
     return dispatch => {
-        axios.get(`${NEWSAPI_BASEURL}top-headlines?country=id&category=${category}&apiKey=${NEWSAPI_KEY}`)
-        .then((res) => {
-            // console.log(res.data)
-            // console.log(res.data.articles)
+        if(navigator.onLine){
+            axios.get(`${NEWSAPI_BASEURL}top-headlines?country=id&category=${category}&apiKey=${NEWSAPI_KEY}`)
+            .then((res) => {
+                // console.log(res.data)
+                // console.log(res.data.articles)
+                localStorage.setItem('news', JSON.stringify(res.data.articles))
+                dispatch({
+                    type: GET_NEWS,
+                    payload: res.data.articles
+                })
+            }).catch(err => console.log(err))
+        } 
+        else {
+            let local = localStorage.getItem('news', JSON.parse('news'))
             dispatch({
                 type: GET_NEWS,
-                payload: res.data.articles
+                payload: local
             })
-        }).catch(err => console.log(err))
+        }
+        
     }
 
 }
